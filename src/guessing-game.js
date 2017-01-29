df826b3c75d8c3756e9a5426eae35742c345e753
SuperKirik <luis-gusman@yandex.ru>
2017-01-30 00:34:38 +0300

    guessing-game/feature: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.highEdge = max;
+        this.lowEdge = min;
+        this.isLower = null;
+        this.value = null;
     }
 
     guess() {
 
+        if (this.isLower !== null) {
+
+            if (this.isLower === true) {
+                this.lowEdge = this.value + 1;
+            } else {
+                this.highEdge = this.value;
+            }
+        }
+        this.value = Math.floor((this.highEdge + this.lowEdge) * 0.5);
+        return this.value;
     }
 
     lower() {
-
+        //Так как в тестах методы lower и greater перепутаны,
+        //то здесь "!true" вместо "true"
+        this.isLower = !true;
     }
 
     greater() {
-
+        //Так как в тестах методы lower и greater перепутаны,
+        //то здесь "!false" вместо "false"
+        this.isLower = !false;
     }
 }
 
